7e8365fb352e303d20b96aa98e8d0c0a0d71bc31
Flavio Ferreira <flaviometalvale@gmail.com>
2016-07-08 06:25:51 -0300

    Fix Provider

--- src/GoCardLessServiceProvider.php
+++ src/GoCardLessServiceProvider.php
@@ -24,7 +24,7 @@ class GoCardLessServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-     
+        $this->package('fhferreira/gocardless', 'fhferreira/gocardless');
     }
 
     /**
